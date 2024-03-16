167d9f582f0d8a7363f3ec15576de7f188a99a5e
Ihor_Ostapovych <iostapovychweb@gmail.com>
2024-03-16 12:33:24 +0200

    chore: clean up

--- index.js
+++ index.js
@@ -8,13 +8,14 @@ async function main() {
     const completion = await openai.chat.completions.create({
         messages: [
             { role: "system", content: "Act as a Senior JavaScript developer" },
-            { role: "user", content: "generate a js code to print 'Hello, world' to the console. Return result as a plan text, with no comments" },
+            { role: "user", content: "generate a js code to print first 20 fiboncci number to the console. Return result as a plain text, with no comments" },
         ],
         model: "gpt-3.5-turbo",
         temperature: 0,
         seed: 100,
     });
 
+    console.log(completion.choices[0].message.content);
     eval(completion.choices[0].message.content);
 }
 
